e513770a62a24d5a341651243f921adfcd0f0b79
Josine Min <josine.min@bristol.ac.uk>
2017-01-16 13:38:27 +0000

    extractN

--- 01.extract_sftp/scripts/extractN.sh
+++ 01.extract_sftp/scripts/extractN.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
+MYDIR="/panfs/panasas01/sscm/epzjlm"
+filename="$MYDIR/repo/godmc_phase1_analysis/01.extract_sftp/data/cohorts.txt"
 
-filename="/panfs/panasas01/sscm/epzjlm/repo/godmc_phase1_analysis/01.extract_sftp/data/cohorts.txt"
+nocohorts=`cat $MYDIR/repo/godmc_phase1_analysis/01.extract_sftp/data/cohorts.txt |wc -l`
 
-nocohorts=`cat /panfs/panasas01/sscm/epzjlm/repo/godmc_phase1_analysis/01.extract_sftp/data/cohorts.txt |wc -l`
-
-echo "user" "cohort" "N" > /panfs/panasas01/sscm/epzjlm/repo/godmc_phase1_analysis/01.extract_sftp/data/cohort_samplesizes.txt
+echo "user" "cohort" "N" > $MYDIR/repo/godmc_phase1_analysis/01.extract_sftp/data/cohort_samplesizes.txt
 
 for i in `seq 1 $nocohorts`;do
 
 user=`head -n ${i} ${filename} | tail -n 1 | cut -d " " -f 1`
 cohort=`head -n ${i} ${filename} | tail -n 1 | cut -d " " -f 2`
+
+if [ $cohort = "Phase1_SCZ" ]; then
+
+N=`grep "people pass filters and QC." /panfs/panasas01/shared-godmc/sftp/GoDMC/${user}/${cohort}/results/04/positive_control_cg07959070.log | cut -d " " -f 4`
+
+elif [ $cohort = "TwinsUK" ]; then
+
+N="833"	
+
+else
+
 N=`grep "people pass filters and QC." /panfs/panasas01/shared-godmc/sftp/GoDMC/${user}/${cohort}/results/04/positive_control_pcadjusted_cg07959070.log | cut -d " " -f 4`
+
+fi
+
 echo $i
 echo $user
 echo $cohort
 echo $N
 
-echo $user $cohort $N >> /panfs/panasas01/sscm/epzjlm/repo/godmc_phase1_analysis/01.extract_sftp/data/cohort_samplesizes.txt
+echo $user $cohort $N >> $MYDIR/repo/godmc_phase1_analysis/01.extract_sftp/data/cohort_samplesizes.txt
 done
 
