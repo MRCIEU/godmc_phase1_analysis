70f2d93aa4c5a1d8bfbe8b98dc5deae88346099d
burrowsk <kimberley.burrows@bristol.ac.uk>
2017-04-10 13:01:19 +0100

    updated file paths

--- 02.extract_counts/scripts/splitcohortintoprobefiles.sh
+++ 02.extract_counts/scripts/splitcohortintoprobefiles.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #PBS -N split
-#PBS -o /panfs/panasas01/shared-godmc/job_report/split-o
-#PBS -e /panfs/panasas01/shared-godmc/job_report/split-e
+#PBS -o /panfs/panasas01/shared-godmc/job_report_2017/split-o
+#PBS -e /panfs/panasas01/shared-godmc/job_report_2017/split-e
 #PBS -l walltime=12:00:00
-#PBS -t 1-12
-# PBS -t 3
+#PBS -t 1-23
+# PBS -t 21
 #PBS -l nodes=1:ppn=8
 #PBS -S /bin/bash
 
@@ -33,7 +33,7 @@ echo $cohort
 
 #merge matrixQTL to AFs
 
-cd /panfs/panasas01/shared-godmc/meta-analysis/inputfiles
+cd /panfs/panasas01/shared-godmc/meta-analysis_2017/inputfiles
 #echo SNP CpG ID BETA SE P |perl -pe 's/ /\t/g' >${user}\_${cohort}.gwama.formatted.txt
 #cat ${user}\_${cohort}.*.gwama.formatted.txt >>${user}\_${cohort}.gwama.formatted.txt
 #awk 'NR>1 {print $2}' <${user}\_${cohort}.gwama.formatted.txt |sort -u >${user}\_${cohort}.probes
@@ -42,7 +42,7 @@ cd /panfs/panasas01/shared-godmc/meta-analysis/inputfiles
 #split -l $(( $( wc -l < ${user}\_${cohort}.probes ) / 50 + 1 )) ${user}\_${cohort}.probes probe.${user}\_${cohort}
 #ls probe.${user}\_${cohort}* >probe.${user}\_${cohort}.files
 zcat /panfs/panasas01/shared-godmc/sftp/GoDMC/$user/$cohort/results/02/data.frq.gz | sed -e 's/[[:space:]]\+/ /g' |perl -pe 's/^ //g'|perl -pe 's/ /\t/g'|awk -v OFS='\t' '{ if(NR>1) print $1,$2,$3,$4,$5,$6/2; else print $0;}'|perl -pe 's/A1/EA/g' |perl -pe 's/A2/NEA/g' |perl -pe 's/MAF/EAF/g'|perl -pe 's/NCHROBS/N/g' |perl -pe 's/ /\t/g'>$cohort.frq.tmp
-perl /panfs/panasas01/shared-godmc/scripts/join_file.pl -i "${user}_${cohort}.gwama.formatted.txt,TAB,0 $cohort.frq.tmp,TAB,1" -o ${user}_${cohort}.gwama.formatted.txt2 -a 1
+perl /panfs/panasas01/sscm/epwkb/GoDMC_Analysis/CpG_SNP_FINAL/repo/godmc_phase1_analysis/02.extract_counts/scripts/join_file.pl -i "${user}_${cohort}.gwama.formatted.txt,TAB,0 $cohort.frq.tmp,TAB,1" -o ${user}_${cohort}.gwama.formatted.txt2 -a 1
 awk -F'\t' '{ if(NR>1) print $0; else print "POS","CpG","ID","BETA","SE","P","CHR","SNP","EA","NEA","EAF","N";}' OFS='\t'<${user}\_${cohort}.gwama.formatted.txt2 | sed 's/\:SNP//1'|sed 's/\:INDEL//1' |sed 's/[^:]*://1' >${user}\_${cohort}.gwama.formatted.txt3
 rm ${user}\_${cohort}.gwama.formatted.txt2
 
@@ -69,7 +69,7 @@ ls ${user}\_${cohort}.*.probes >probe.${user}\_${cohort}.files
 #gzip ${user}\_${cohort}.gwama.formatted.txt
 #mkdir -p $cohort
 #cd $cohort
-#awk 'BEGIN {FS = "\t"}; {print > ("'$user'_'${cohort}.'"$2".txt")}' </panfs/panasas01/shared-godmc/meta-analysis/inputfiles/${user}\_${cohort}.gwama.formatted.txt
+#awk 'BEGIN {FS = "\t"}; {print > ("'$user'_'${cohort}.'"$2".txt")}' </panfs/panasas01/shared-godmc/meta-analysis_2017/inputfiles/${user}\_${cohort}.gwama.formatted.txt
 
 #split -l $(( $( wc -l < ${user}\_${cohort}.probes ) / 100 + 1 )) ${user}\_${cohort}.probes probe.${user}\_${cohort}
 #ls probe.${user}\_${cohort}* >probe.${user}\_${cohort}.files
