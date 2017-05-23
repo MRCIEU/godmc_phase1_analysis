94b976971d21db2778dfcd1b780449c1ba322cc3
Josine Min <josine.min@bristol.ac.uk>
2017-05-23 12:31:48 +0100

    updated zhou

--- 07.snp_cpg_selection/scripts/get_zhou_cpgs.r
+++ 07.snp_cpg_selection/scripts/get_zhou_cpgs.r
@@ -8,8 +8,20 @@ library(data.table)
 
 # Comprehensive characterization, annotation and innovative use of Infinium DNA methylation BeadChip probes
 
-a <- fread("EPIC.anno.GRCh38.tsv", header=TRUE)
-feat <- meffil.get.features()
-b <- subset(a, probeID %in% feat$name & !MASK.general)
-write.table(b$probeID, "../data/retain_from_zhou.txt", row=F, col=F, qu=F)
+#a <- fread("EPIC.anno.GRCh38.tsv", header=TRUE)
+#feat <- meffil.get.features()
+#b <- subset(a, probeID %in% feat$name & !MASK.general)
+#write.table(b$probeID, "../data/retain_from_zhou.txt", row=F, col=F, qu=F)
 
+
+#http://zwdzwd.io/InfiniumAnnotation/current/hm450/hm450.manifest.tsv.gz
+
+a <- fread("hm450.manifest.tsv.gz", header=TRUE)
+#feat <- meffil.get.features()
+b <- subset(a, probeID %in% feat$name & !MASK.general)
+#write.table(b$probeID, "../data/retain_from_zhou.txt", row=F, col=F, qu=F)
+
+#table(r$MASK.general)
+
+# FALSE   TRUE 
+#435391  50186 
